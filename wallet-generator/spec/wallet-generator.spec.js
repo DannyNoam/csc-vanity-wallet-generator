6bff138b373e1c4cf00c199ca134f4fa1979d5a5
danny.noam <danny.noam@gamesys.co.uk>
2018-06-16 14:27:34 +0100

    [WalletGenerator] Amended assertions and removed Sinon as dependency.

--- wallet-generator/spec/wallet-generator.spec.js
+++ wallet-generator/spec/wallet-generator.spec.js
@@ -1,19 +1,27 @@
 import WalletGenerator from "../src/wallet-generator";
-import sinon from 'sinon'
 
 describe('Given the wallet generator', () => {
+  let mockAddressPayload = {
+    secret: 'secret',
+    address: 'address',
+    publicKey: 'publicKey'
+  };
+  
   let cscApi = {
-    generateAddress: sinon.spy()
+    generateAddress: function () {
+      return mockAddressPayload;
+    }
   };
   let testObj = new WalletGenerator(cscApi);
   
   describe('when the generate function is invoked', () => {
-    let numberOfAddresses = 5;
+    let numberOfAddresses = 3;
     
-    testObj.generate(numberOfAddresses);
+    let addresses = testObj.generate(numberOfAddresses);
     
     it('should generate exactly the number of addresses that it was requested to', () => {
-      sinon.assert.callCount(cscApi.generateAddress, numberOfAddresses);
+      expect(addresses.length).toBe(3);
+      expect(addresses).toEqual([mockAddressPayload, mockAddressPayload, mockAddressPayload]);
     });
   });
 });
