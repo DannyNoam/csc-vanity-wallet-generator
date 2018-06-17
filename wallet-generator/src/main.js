15e768b3f8638dea09564323a533d99d81d104e6
danny.noam <danny.noam@gamesys.co.uk>
2018-06-17 21:04:19 +0100

    [WalletGenerator] Main script now infinitely loops to create vanity wallets.

--- wallet-generator/src/main.js
+++ wallet-generator/src/main.js
@@ -1,8 +1,8 @@
-import VanityWalletGenerator from 'vanity-wallet-generator';
-import VanityWalletStorer from 'vanity-wallet-storer';
+import VanityWalletGenerator from './vanity-wallet-generator';
+import VanityWalletStorer from './vanity-wallet-storer';
 
 const SERVICE_ENDPOINT = "http://tbc";
-const WALLET_BATCH_SIZE = 10;
+const WALLET_BATCH_SIZE = 1;
 
 let CasinocoinAPI = require('casinocoin-libjs').CasinocoinAPI;
 let cscApi = new CasinocoinAPI({ server: 'wss://ws01.casinocoin.org:4443' });
@@ -10,5 +10,11 @@ let words = require('an-array-of-english-words');
 let request = require('request');
 
 let vanityWalletGenerator = new VanityWalletGenerator(cscApi, words);
-let vanityWalletStorer = new VanityWalletStorer(SERVICE_ENDPOINT, vanityAddressGenerator, request, WALLET_BATCH_SIZE);
+let vanityWalletStorer = new VanityWalletStorer(SERVICE_ENDPOINT, vanityWalletGenerator, request, WALLET_BATCH_SIZE);
+
+console.log("CasinoCoin Vanity Wallet Generator v1.0.0, by Danny Noam");
+
+while(true) {
+  vanityWalletStorer.generateAndStoreVanityWallets();
+}
 
