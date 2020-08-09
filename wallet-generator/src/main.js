af5a759aa945a7b78c363062dca0b718cea6137d
Danny Noam <Danny.Noam@Gamesys.co.uk>
2020-08-09 20:15:57 +0100

    Increased redundancy, and minor refactoring.

--- wallet-generator/src/main.js
+++ wallet-generator/src/main.js
@@ -11,30 +11,42 @@ let CasinocoinAPI = require('casinocoin-libjs').CasinocoinAPI;
 let cscApi = new CasinocoinAPI({ server: 'wss://ws01.casinocoin.org:4443' });
 let walletApi = new WalletAPI(cscApi);
 let request = require('request');
-let words = require('an-array-of-english-words');
+let englishWords = require('an-array-of-english-words');
 let frenchWords = require('an-array-of-french-words');
 let spanishWords = require('an-array-of-spanish-words');
 let names = require('human-names');
 let badWords = require('badwords-list');
 
-for(let i = 0; i < frenchWords.length; i++){
-    words.push(frenchWords[i]);
+const assembleWordsList = () => {
+    let words = [];
+
+    /* For loops required because arrays are huge;
+       without them, we run out of frames. Try it.
+     */
+    for (let i = 0; i < englishWords.length; i++) {
+        words.push(englishWords[i]);
+    }
+
+    for (let i = 0; i < frenchWords.length; i++) {
+        words.push(frenchWords[i]);
+    }
+
+    for (let i = 0; i < spanishWords.length; i++) {
+        words.push(spanishWords[i]);
+    }
+
+    words.push(...names.allEn);
+    words.push(...names.allNl);
+    words.push(...names.allEs);
+    words.push(...names.allDe);
+    words.push(...names.allFr);
+    words.push(...names.allIt);
+    words.push(...badWords.array);
+
+    return words;
 }
 
-for(let i = 0; i < spanishWords.length; i++){
-    words.push(spanishWords[i]);
-}
-
-words.push(...names.allEn);
-words.push(...names.allNl);
-words.push(...names.allEs);
-words.push(...names.allDe);
-words.push(...names.allFr);
-words.push(...names.allIt);
-words.push(...badWords.array);
-
-console.log(words.length);
-
+let words = assembleWordsList();
 let vanityWalletGenerator = new VanityWalletGenerator(walletApi, words, MINIMUM_WORD_LENGTH);
 let vanityWalletStorer = new VanityWalletStorer(SERVICE_ENDPOINT, vanityWalletGenerator, request);
 
